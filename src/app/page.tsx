b20330a86a6519fe4be11530b3c686c00b178645
Danillo Ferreira <nilloferreiira@gmail.com>
2026-09-13 18:21:27 -0300

    feat: add skeleton loaders for experiences and projects sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import { Education } from "@/components/education/education"
 import { ExperienceContainer } from "@/components/experiences/experiences-container"
 import { Header } from "@/components/header/header"
 import { SwitchLanguage } from "@/components/language/switch"
-import { LoadingSpinner } from "@/components/loading/loading"
 import { Now } from "@/components/now/now"
 import { ProjectContainer } from "@/components/projects/projects-container"
 import { Stack } from "@/components/stack/stack"
@@ -16,7 +15,6 @@ import { useProjects } from "@/hooks/projects/useProjects"
 export default function Home() {
 	const { data: projects, isLoading: isLoadingProjects } = useProjects()
 	const { data: experiences, isLoading: isLoadingExperiences } = useExperiences()
-	const isLoading = isLoadingProjects || isLoadingExperiences
 	return (
 		<div className="min-h-screen relative">
 			<div className="fixed top-6 right-6 z-50">
@@ -26,14 +24,8 @@ export default function Home() {
 			<main className="w-full flex flex-col">
 				<About />
 				<Stack />
-				{isLoading ? (
-					<LoadingSpinner />
-				) : (
-					<>
-						<ExperienceContainer experiences={experiences!} />
-						<ProjectContainer projects={projects!} />
-					</>
-				)}
+				<ExperienceContainer experiences={experiences ?? []} isLoading={isLoadingExperiences} />
+				<ProjectContainer projects={projects ?? []} isLoading={isLoadingProjects} />
 				<Education />
 				<Now />
 				<Contact />
